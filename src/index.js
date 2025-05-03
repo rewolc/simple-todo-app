cb82614eac64c17c6147ceabdb5de0dcbfbfcacf
belyaev.egor6 <belyaev.egor6@wildberries.work>
2025-05-03 16:22:22 +0300

    feat: upd packages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom";
 //component file
 import TodoContainer from "./components/TodoContainer";
 import "./App.css";
+import {createRoot} from "react-dom/client";
 
-ReactDOM.render(<TodoContainer />, document.getElementById("root"));
+const root = createRoot(document.getElementById('root'));
+
+root.render(<TodoContainer />);
